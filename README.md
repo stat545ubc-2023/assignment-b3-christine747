e34e778406346d907c3e358b21d72dc6a58cb553
christine747 <christinechen747@gmail.com>
2023-12-05 09:52:44 -0800

    assignment b4

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # Palmer Penguins App
 
-[Link to run the app](https://christinec.shinyapps.io/palmerpenguinsapp/) 
+[Link to updated version of the app](https://christinec.shinyapps.io/palmerpenguinsapp/) 
 
+
+[Link to original version of the app](https://christinec.shinyapps.io/palmerpenguinsapp/)
 ## Overview
 
-The Palmer Penguin App can be used as an exploratory tool or for research purposes that involve looking at penguins that live on the islands Biscoe, Dream, and Torgensen within the Palmer Archipelago of Antarctica between the years 2007 to 2009. The penguin species include Adelie, Chinstrap, and Gentoo. Users can view results based on selected species, islands, various ranges for bill depth, and flipper length. It also helps visualize the data in a histogram corresponding to the body mass of the penguins. Users can also interact with the results table below the graph. 
+The Palmer Penguin App can be used as an exploratory tool or for research purposes that involve looking at penguins that live on the islands Biscoe, Dream, and Torgensen within the Palmer Archipelago of Antarctica between the years 2007 to 2009. The penguin species include Adelie, Chinstrap, and Gentoo. Users can view results based on selected species, islands, various ranges for bill depth, bill length, and flipper length. It also helps visualize the data in a histogram corresponding to the body mass of the penguins. Users can also interact with the results table below the graph and download the table in a csv file for further analysis. 
 
 ### App Features
 
 1. **Bill depth and Flipper length range selection**: Users can select the desired range for penguin bill depth and flipper length measured in mm.
 
-2. **Species selection**: Users can select to view measurement results of their desired penguin species. 
+2. **Bill length range input selection**: Users can input the desired numeric range for penguin bill length with a minimum and maximum length measured in mm.
 
-3. **Island selection**: Users have the option to filter results by islands or have a broad overview by selecting the "all islands" option.
+3. **Species selection**: Users can select to view measurement results of their desired penguin species. 
 
-4. **Interactive results table**: At the bottom of the graph, there is an interactive table that shows the all the results including more information on each individual penguin. 
+4. **Island selection**: Users have the option to filter results by islands or have a broad overview by selecting the "all islands" option.
+
+5. **Display of total number of penguins**: At the top of the graph, users are shown the total number of penguins that match their search criteria. Will display 0 if there are no results found. 
+
+6. **Graph of filtered penguins by body mass**: A histogram that shows the distribution of body mass in grams of the filtered penguins searched.
+
+7. **Graph of filtered penguins by bill size**: A scatter plot that shows the bill size of penguins by their bill length and depth. 
+
+8. **Graphs organized under tabs**: Graphs generated from the filtered search are organized under their respective tabs that users can select to view. 
+
+9. **Interactive table of results**: At the bottom of the graph, there is an interactive table that shows the all the results including more information on each individual penguin. 
+
+10. **Downloadable results**: Users can download their search results in a csv file to do more analysis on their own. The downloaded table contains additional information related to the sex of the penguin and the year the data was collected. Users can choose to keep or delete the additional information or include them in their analysis.  
 
 
 ## Data Source
--- shinyapp/app.R
+++ shinyapp/app.R
@@ -2,20 +2,29 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(DT)
+library(shinythemes)
 
 penguindata <- read.csv("penguins.csv", stringsAsFactors = FALSE)
 
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("united"),
   titlePanel("Palmer Penguins App"),
   img(src = "penguin.png"), # added an image of palmerpenguins
-  h4("Search Filters:"),
+  h4("Explore penguins that live within the Palmer Archipelago of Antarctica:"),
+  br(),
   sidebarLayout(
     sidebarPanel(
+      h4("Apply search filters here:"),
       # added 2 options for users to select the range for bill depth and flipper length. This can be helpful for users if they want to look at a specific
       # range of bill depth or flipper length for a particular species, or explore the different ranges for different species. 
       sliderInput("billdepthInput", "Bill Depth", 0, 80, c(0, 40), post = "mm"),
       sliderInput("flipperInput", "Flipper Length", 150, 300, c(150, 200), post = "mm"),  
       
+      # added the option for users to input a minimum and maximum bill length using numeric values.This can be helpful when users want to input very 
+      # specific measurements down to the decimal place. 
+      numericInput("billlengthMinInput", "Input Minimum Bill Length in mm", min = 0, value = 0),
+      numericInput("billlengthMaxInput", "Input Maximum Bill Length in mm", min = 0, value = max(penguindata$bill_length_mm, na.rm = TRUE)),
+      span("**59.6 mm is the maximum bill length value within this dataset**"),
+      br(), br(),
       # added option to select different penguin species. Users have 3 species to choose from. This helps user narrow their search down when trying to look at species specific traits.
       radioButtons("speciesInput", "Species",
                    choices = c("Adelie", "Gentoo", "Chinstrap"),
@@ -25,12 +34,24 @@ ui <- fluidPage(
       # added the option for "all" islands so that user doesn't always have to filter by a single island and can get a broad overview.
       selectInput("islandInput", "Filter by Island", 
                   choices = c("All islands", unique(penguindata$island))), 
+      
+      # added the option for users to download the results from the filtered search. This is helpful if users want to use the data for their own analysis
+      # and can have the data all in one table in a csv file. 
+      downloadButton("table_download", "Download Table Results"), # added a downloadable table that contains the content the user searched for.
+      br(), br(),
+      span("Data source:", 
+           tags$a("palmerpenguins",
+                  href = "https://allisonhorst.github.io/palmerpenguins/")),
+      br(),
+      span("Artwork by @allison_horst")
     ),
     mainPanel(
       # added summary text at the top of the main panel that shows the total number of results displayed in the graph and in the table. 
       # This helps the user see how many penguins match the filters that they have put in. 
       h3(textOutput("summaryText")),
-      plotOutput("histoplot"),
+       tabsetPanel(
+        tabPanel("Histogram",h3("Penguin Body Mass Distribution"), p("A histogram of the filtered penguins by their body mass"), plotOutput("histoplot")),
+        tabPanel("Scatter Plot", h3 ("Bill Size Scatterplot"), p("A scatter plot of the filtered penguin bill sizes based on their bill length and depth"), plotOutput("scatterPlot"))),
       br(),
       DTOutput("results")
     )
@@ -43,23 +64,33 @@ server <- function(input, output) {
       filter(bill_depth_mm >= input$billdepthInput[1],
              bill_depth_mm <= input$billdepthInput[2],
              species == input$speciesInput,
+             bill_length_mm >= input$billlengthMinInput,
+             bill_length_mm <= input$billlengthMaxInput,
              flipper_length_mm >= input$flipperInput[1],
              flipper_length_mm <= input$flipperInput[2])
     
-      if (input$islandInput != "All islands") {
+    if (input$islandInput != "All islands") {
       filtered_data <- filtered_data %>%
         filter(island == input$islandInput)
     }
-      else 
-    filtered_data
+    else 
+      filtered_data
   })
   
   # histogram that plots the filtered penguins by their body mass in grams. Different colors represent different islands. 
   output$histoplot <- renderPlot({
-      ggplot(penguin_filtered(), aes(x = body_mass_g, fill = island)) +
+    ggplot(penguin_filtered(), aes(x = body_mass_g, fill = island)) +
       geom_histogram(binwidth = 20) +
       xlab("Body Mass (g)") +
       ylab("Count")
+  })
+  
+  # scatter plot for bill depth
+  output$scatterPlot <- renderPlot({
+    ggplot(penguin_filtered(), aes(x = bill_length_mm, y = bill_depth_mm, color = island)) +
+      geom_point(size = 4) +
+      xlab("Bill length (mm)") +
+      ylab("Bill depth (mm)")
   })
   
   # added an interactive table that comes from the filtered content the user selects. 
@@ -69,10 +100,26 @@ server <- function(input, output) {
         `Species` = species,
         `Island` = island,
         `Bill Depth` = bill_depth_mm,
+        `Bill Length` = bill_length_mm,
         `Flipper Length` = flipper_length_mm
       )
   })
   
+  output$summaryText <- renderText({
+    numPenguins <- nrow(penguin_filtered())
+    if (is.null(numPenguins)) {
+      numPenguins <- 0
+    }
+    paste0("We found ", numPenguins, " penguins that match your filters!")
+    
+  })
+  
+  output$table_download <- downloadHandler( 
+    filename = "palmerpenguins-results.csv",
+    content = function(file) {
+      write.csv(penguin_filtered(), file)
+    }
+  )
 }
 
 
